fcc04b1f5fa0dc59882e77a2818b6a00b9ea7e4a
kent7k <kent.ki720@gmail.com>
2023-02-25 19:48:23 +0900

    a

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -126,9 +126,8 @@ Rails.application.configure do
 
   config.middleware.use ExceptionNotification::Rack,
     :email => {
-     :email_prefix => "[PREFIX] ",
-     :sender_address => %{"notifier" <notifier@example.com>},
-     :exception_recipients => %w{exceptions@example.com}
-  }
-
+      :email_prefix         => "[PREFIX] ",
+      :sender_address       => %{"notifier" <notifier@example.com>},
+      :exception_recipients => %w{exceptions@example.com}
+    }
 end
