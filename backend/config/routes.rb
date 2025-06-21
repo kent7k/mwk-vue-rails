5d3bdea4cf08945956ecfb98cf901da42ca5ba89
kent7k <kento@MacBook-Air.local>
2025-06-21 17:25:14 +0900

    Default Commit Message

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,29 +1,33 @@
 Rails.application.routes.draw do
-  post "/graphql", to: "graphql#execute"
-  devise_for :users
+  post '/graphql', to: 'graphql#execute'
+
+  # Web 版の認証が不要ならコメントアウト
+  # devise_for :users
 
   namespace :api do
-    namespace :v1, format: 'json' do
+    namespace :v1, format: :json do
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-        registrations: 'api/v1/auth/registrations'
+        registrations: 'auth/registrations'
       }
-      get 'isLike', to: 'post_likes#isLike'
-      get 'isJoin', to: 'post_joins#isJoin'
-      get 'allpost', to: 'posts#allpost'
-      get 'new_post', to: 'posts#new_post'
-      get 'new_plan_post', to: 'posts#new_plan_post'
-      resources :posts, only: %i[index show create update destroy] do
+
+      get  'isLike',         to: 'post_likes#isLike'
+      get  'isJoin',         to: 'post_joins#isJoin'
+      get  'allpost',        to: 'posts#allpost'
+      get  'new_post',       to: 'posts#new_post'
+      get  'new_plan_post',  to: 'posts#new_plan_post'
+
+      resources :posts,  only: %i[index show create update destroy] do
         get :search, on: :collection
       end
-      resources :users, only: %i[index show create destroy] do
+      resources :users,  only: %i[index show create destroy] do
         get :search, on: :collection
       end
-      resources :messages, only: %i[index create update destroy]
-      resources :pickups, only: %i[index create destroy]
-      resource :relationships, only: %i[create destroy]
-      resource :post_likes, only: %i[create destroy]
-      resource :post_joins, only: %i[create destroy]
-      resource :message_likes, only: %i[create destroy]
+      resources :messages,      only: %i[index create update destroy]
+      resources :pickups,       only: %i[index create destroy]
+      resource  :relationships, only: %i[create destroy]
+      resource  :post_likes,    only: %i[create destroy]
+      resource  :post_joins,    only: %i[create destroy]
+      resource  :message_likes, only: %i[create destroy]
 
       get 'health_check', to: 'health_check#index'
     end
--- frontend/plugins/axios.js
+++ frontend/plugins/axios.js
@@ -1,30 +1,50 @@
-export default function({ $axios }) {
-  // $axios.setToken("access_token")
+export default function ({ $axios, redirect }) {
+  // ---------- リクエスト ----------
   $axios.onRequest((config) => {
-    console.log('-------- start /plugins/axios.js------------')
-    config.headers.client = window.localStorage.getItem('client')
-    config.headers['access-token'] = window.localStorage.getItem('access-token')
-    config.headers.uid = window.localStorage.getItem('uid')
-    config.headers['token-type'] = window.localStorage.getItem('token-type')
-    config.headers.expiry = window.localStorage.getItem('expiry')
-    config.headers['content-type'] = 'application/json'
-    console.log(config)
+    // SSR 中は window が無いのでガード
+    if (process.client) {
+      const token   = localStorage.getItem('access-token')
+      const client  = localStorage.getItem('client')
+      const uid     = localStorage.getItem('uid')
+      const expiry  = localStorage.getItem('expiry')
+      const type    = localStorage.getItem('token-type')
+
+      // null を送らない。存在するものだけ載せる
+      if (token && client && uid) {
+        Object.assign(config.headers, {
+          'access-token': token,
+          client,
+          uid,
+          expiry,
+          'token-type': type,
+        })
+      }
+    }
+    config.headers['Content-Type'] = 'application/json'
+    return config
   })
 
+  // ---------- レスポンス ----------
   $axios.onResponse((response) => {
-    $axios.setHeader('Access-Control-Allow-Origin', '*')
-    console.log('⇩response')
-    console.log(response)
-    if (response.headers.client) {
-      console.log('認証情報あり。  /plugins/axios.js')
+    // CORS ヘッダをクライアント側で付けても無意味なので削除
+    // $axios.setHeader('Access-Control-Allow-Origin', '*')  ←不要
+
+    if (process.client && response.headers.client) {
       localStorage.setItem('access-token', response.headers['access-token'])
-      localStorage.setItem('client', response.headers.client)
-      localStorage.setItem('uid', response.headers.uid)
-      localStorage.setItem('token-type', response.headers['token-type'])
-      localStorage.setItem('expiry', response.headers.expiry)
-    } else {
-      console.log('認証情報を発見できませんでした')
+      localStorage.setItem('client',       response.headers.client)
+      localStorage.setItem('uid',          response.headers.uid)
+      localStorage.setItem('token-type',   response.headers['token-type'])
+      localStorage.setItem('expiry',       response.headers.expiry)
     }
-    console.log('-------- end /plugins/axios.js------------')
+    return response
+  })
+
+  // ---------- エラー ----------
+  $axios.onError((error) => {
+    if (error.response?.status === 401) {
+      // トークン失効時の再ログイン誘導など
+      redirect('/login')
+    }
+    return Promise.reject(error)
   })
 }
